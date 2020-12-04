6ec1e8993cf5e7a525bc39147d231e381f4118cb
Klim Romanov <klr0manov@yandex.ru>
2020-12-04 13:57:49 +0500

    Factorial

---
+++ Programm1.pas
@@ -0,0 +1,17 @@
+﻿function factorial(n: integer): integer;
+begin
+  if (n = 0) or (n = 1) then
+    factorial := 1
+  else
+    factorial := factorial(n - 1) * n;
+end;
+
+var
+  y, n: integer;
+
+begin
+  writeln('введите n:');
+  readln(n);
+  y := factorial(n) / (1 + factorial(n));
+  writeln('y=', y);
+end.
